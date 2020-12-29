9e2365a7944c2634a8ea1c91a8125d131c52f813
Boeun05 <71836751+Boeun05@users.noreply.github.com>
2020-12-30 00:54:48 +0900

    input name 수정

--- velopert/input/src/App.js
+++ velopert/input/src/App.js
@@ -3,19 +3,29 @@ import './App.css';
 
 function App() {
   const [inputs, setInputs] = useState({
-    name: '',
-    nickname: ''
+    name: '보은',
+    nickname: '송'
   })
 
+  
+
   const {name, nickname} = inputs
+  
+  
 
   const onChange = (e) => {
     const {value, name} = e.target
+    
     setInputs({
       ...inputs,
       [name]: value
     })
+    console.log(inputs)
+    
   }
+  
+  
+  
 
   const onReset = () => {
     setInputs({
@@ -26,8 +36,8 @@ function App() {
 
   return (
     <div>
-      <input placeholder='이름' onChange = {onChange} value = {name}/>
-      <input placeholder='닉네임' onChange = {onChange} value = {nickname}/>
+      <input name="name" placeholder='이름' onChange = {onChange} value = {name}/>
+      <input name="nickname" placeholder='닉네임' onChange = {onChange} value = {nickname}/>
       <button onClick = {onReset}>초기화</button>
       <div>
         <b>값</b>
@@ -36,5 +46,6 @@ function App() {
     </div>
   )
 }
+
 
 export default App;
