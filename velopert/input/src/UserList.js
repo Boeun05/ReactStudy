04e2fc2bb1ca455c1c31f532ecaab124ca79e5c1
Boeun05 <71836751+Boeun05@users.noreply.github.com>
2020-12-30 16:44:37 +0900

    useEffect(deps파라미터생략)

--- velopert/input/src/UserList.js
+++ velopert/input/src/UserList.js
@@ -2,13 +2,8 @@ import React, { useEffect } from 'react'
 
 function User({user, onRemove, onToggle}) {
     useEffect(() => {
-        console.log('user 값이 설정됨')
         console.log(user)
-        return () => {
-            console.log('user가 바뀌기 전..')
-            console.log(user) //clean up
-        }
-    },[user])
+    },) //deps 파라미너를 생략하면 컴포넌트가 리렌더링 될 때 마다 호출됨
     return (
       <div>
         <b
