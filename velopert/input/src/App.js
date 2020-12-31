10e901f9efa6fbdf9f9d4c858ed278a48281c8b0
Boeun05 <71836751+Boeun05@users.noreply.github.com>
2020-12-31 11:49:21 +0900

    useReducer

--- velopert/input/src/App.js
+++ velopert/input/src/App.js
@@ -1,103 +1,35 @@
-import React, { useRef, useState, useMemo, useCallback } from 'react';
-import UserList from './UserList';
-import CreateUser from './CreateUser';
+import React, { useState, useReducer} from 'react';
 
-function countActiveUsers(users) {
-  console.log('활성 사용자 수를 세는중...');
-  return users.filter((user) => user.active).length;
+function reducer(state, action){
+  switch(action.type){
+    case 'INCREMENT': 
+      return state +1
+    case 'DECREMENT':
+      return state -1
+    default:
+      return state
+  }
 }
 
-function App() {
-  const [inputs, setInputs] = useState({
-    username: '',
-    email: '',
-  });
-  const { username, email } = inputs;
-  const onChange = useCallback(
-    (e) => {
-      const { name, value } = e.target;
-      setInputs(inputs => ({
-        ...inputs,
-        [name]: value,
-      }))
-    },[]);
-  const [users, setUsers] = useState([
-    {
-      id: 1,
-      username: 'velopert',
-      email: 'public.velopert@gmail.com',
-      active: true,
-    },
-    {
-      id: 2,
-      username: 'tester',
-      email: 'tester@example.com',
-      active: false,
-    },
-    {
-      id: 3,
-      username: 'liz',
-      email: 'liz@example.com',
-      active: false,
-    },
-  ]);
+function Counter() {
+  //const [state, dispatch] = useReducer(reducer, initialState);
+  const [number, dispatch] = useReducer(reducer, 0)
 
-  const nextId = useRef(4);
-  const onCreate = useCallback(() => {
-    console.log('onCreate')
-    const user = {
-      id: nextId.current,
-      username,
-      email,
-    };
-    setUsers(users => users.concat(user));
+  const onIncrese = () => {
+    dispatch({type: 'INCREMENT'})
+  }
 
-    setInputs({
-      username: '',
-      email: '',
-    });
-    nextId.current += 1;
-  }, [username, email]);
+  const onDecrease = () => {
+    dispatch({type: 'DECREMENT'})
+  }
 
-  const onRemove = useCallback(
-    (id) => {
-      // user.id 가 파라미터로 일치하지 않는 원소만 추출해서 새로운 배열을 만듬
-      // = user.id 가 id 인 것을 제거함
-      setUsers(users => users.filter((user) => user.id !== id));
-      console.log('onRemove')
-    },
-    [],
-  );
-  const onToggle = useCallback(
-    (id) => {
-      setUsers(
-        users => 
-        users.map((user) =>
-          user.id === id ? { ...user, active: !user.active } : user,
-        ),
-      );
-    },
-    [],
-  );
-  /*use memo는 특정 결과값을 재사용 할 때 사용
-  useMemo 의 첫번째 파라미터에는 어떻게 연산할지 정의하는 함수를 넣어주면 되고 
-  두번째 파라미터에는 deps 배열을 넣어주면 되는데,
-   이 배열 안에 넣은 내용이 바뀌면, 
-   우리가 등록한 함수를 호출해서 값을 연산해주고, 
-   만약에 내용이 바뀌지 않았다면 이전에 연산한 값을 재사용하게 됩니다.*/
-  const count = useMemo(() => countActiveUsers(users), [users]);
   return (
-    <>
-      <CreateUser
-        username={username}
-        email={email}
-        onChange={onChange}
-        onCreate={onCreate}
-      />
-      <UserList users={users} onRemove={onRemove} onToggle={onToggle} />
-      <div>활성사용자 수 : {count}</div>
-    </>
-  );
+    <div>
+      <h1>{number}</h1>
+      <button onClick = {onIncrese}>+1</button>
+      <button onClick = {onDecrease}>-1</button>
+    </div>
+  )
 }
 
-export default App;
+export default Counter;
